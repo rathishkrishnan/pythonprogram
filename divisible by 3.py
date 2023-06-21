5712906c70520b0dd3c873c45b761c98fac6eeaa
jai <jaichandiru95@gmail.com>
2023-06-21 20:03:19 +0530

    functions programs completed

--- divisible by 3.py
+++ divisible by 3.py
@@ -3,3 +3,4 @@ if(num%3==0):
     print("the given number is divisible by 3")
 else:
     print("the given number is not divisible by 3")
+print("program completed")
---
+++ function circle.py
@@ -0,0 +1,6 @@
+r=float(input("enter a radius:"))
+pie=3.14
+def areaperimeter(a):
+    print("area of a circle =",pie*(a)**2)
+    print("perimeter of a cirle =",2*pie*a)
+areaperimeter(r)
