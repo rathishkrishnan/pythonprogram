1887533883940d62c936b81c1339b384e8504207
rathishkrishnan <137188458+rathishkrishnan@users.noreply.github.com>
2023-06-21 19:41:24 +0530

    program completed on(21 June 2023)

---
+++ function rectange.py
@@ -0,0 +1,6 @@
+l=int(input("enter a length:"))
+b=int(input("enter a breadth:"))
+def areaperimeter(x,y):
+    print("the area of a rectange is",x*y)
+    print("the perimeter of a rectange is",2*(l+b))
+areaperimeter(l,b)    
